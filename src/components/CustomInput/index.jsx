39fb5032480e4fca97f52733240c1fc79ad14472
ErikeRJK <erike.dev@gmail.com>
2025-12-09 22:01:24 -0300

    add components Button and input

--- src/components/CustomInput/index.jsx
+++ src/components/CustomInput/index.jsx
@@ -1,17 +1,36 @@
-export default function CustomInput() {
+export default function CustomInput({ label, type, placeholder, icon, rightIcon }) {
+  const id = label.toLowerCase().trim().replace(/ /g, "-")
+
   return (
     <div className="space-y-2 flex flex-col">
-      <label htmlFor="cardNumber" className="text-sm text-[#121721]">
-        Número do cartão
+      <label htmlFor={id} className="text-sm text-[#121721]">
+        {label}
       </label>
-      <input
-        type="text"
-        id="cardNumber"
-        placeholder="0000 0000 0000 0000"
-        className="h-12 font-mono w-[50%] text-base bg-white 
-                    border border-[#e5e7eb] rounded-md py-2 px-3 
-                    outline-none "
-      />
+    
+      <div className="relative w-full">
+        <input
+          type={type}
+          id={id}
+          placeholder={placeholder}
+          className={`h-12 font-mono text-base w-full
+                    bg-white border border-[#e5e7eb]
+                    rounded-md py-2 px-3 outline-none ${icon && "pl-10"}`}
+        />
+
+        {icon && (
+          <div className="absolute left-3 top-1/2 -translate-y-1/2
+           text-[#737b8c] text-2xl">
+            {icon}
+          </div>
+        )}
+
+        {rightIcon && (
+          <div className="absolute right-3 top-1/2 -translate-y-1/2
+           text-[#737b8c] text-2xl">
+            {rightIcon}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
--- src/components/PaymentForm.jsx
+++ src/components/PaymentForm.jsx
@@ -1,9 +1,37 @@
+import { CiCreditCard1 } from "react-icons/ci";
+import { BiLock } from "react-icons/bi";
 import CustomInput from "./customInput";
+import CustomButton from "./CustomButton";
 
 export default function PaymentForm() {
   return (
     <form className="space-y-6">
-      <CustomInput/>
+      <CustomInput
+        label="Número do cartão"
+        type="text"
+        placeholder="0000 0000 0000 0000"
+        icon={<CiCreditCard1 />}
+      />
+
+      <CustomInput
+        label="Nome do titular"
+        type="text"
+        placeholder="Nome do títular do cartão"
+      />
+
+      <div className="grid grid-cols-2 gap-4">
+        <CustomInput label="Validade" type="text" placeholder="MM/AA" />
+
+        <CustomInput
+          label="CVV"
+          type="text"
+          placeholder="•••"
+          rightIcon={<BiLock />}
+        />
+      </div>
+      <CustomButton>
+        Pagar Agora
+      </CustomButton>
     </form>
   );
 }
